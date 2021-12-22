fbc4969b4b2a3de2f69cc7f426714456ea6b60a9
Yuri Scherbachenko <y.scherbachenko@folga.com.ua>
2021-12-22 23:29:02 +0200

    create tv screen animation

--- src/shaders/picture/fragment.glsl
+++ src/shaders/picture/fragment.glsl
@@ -8,17 +8,17 @@ varying vec2 vUv;
 void main() {
   vec2 st = vUv.xy;
 
-  vec3 color = vec3(0.0);
+  vec3 color = vec3(1.0);
 
   // bottom-left
-  vec2 bl = step(vec2(0.1),st);
-  float pct = bl.x * bl.y;
+  // vec2 bl = step(vec2(0.1),st);
+  // float pct = bl.x * bl.y;
 
-  // top-right
-  vec2 tr = step(vec2(0.1),1.0-st);
-  pct *= tr.x * tr.y;
+  // // top-right
+  // vec2 tr = step(vec2(0.1),1.0-st);
+  // pct *= tr.x * tr.y;
 
-  color = vec3(pct);
+  // color = vec3(pct);
     
   gl_FragColor = vec4( color, 1.0 );
 }
